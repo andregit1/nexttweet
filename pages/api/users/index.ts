fce7c60302e4e5123866e620ffc972acfb59ad02
andregit1 <andreaswidjaya1@gmail.com>
2023-04-10 21:53:21 +0700

    bugfix 404 userId

--- pages/api/users/index.ts
+++ pages/api/users/index.ts
@@ -1,7 +1,6 @@
 import { NextApiRequest, NextApiResponse } from "next";
 import bcrypt from "bcrypt"
 import prisma from "@/libs/prismadb"
-import serverAuth from "@/libs/serverAuth";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method !== 'GET') return res.status(405).end()
--- pages/users/[userId].tsx
+++ pages/users/[userId].tsx
@@ -1,17 +1,17 @@
+import { useRouter } from 'next/router';
+import { ClipLoader } from 'react-spinners';
+
 import Header from '@/components/Header';
 import UserBio from '@/components/users/UserBio';
 import UserHero from '@/components/users/UserHero';
 import useUser from '@/hooks/useUser';
-import { useRouter } from 'next/router';
-import React from 'react';
-import { ClipLoader } from 'react-spinners';
 
 const UserView = () => {
   const router = useRouter()
   const { userId } = router.query
-
+  console.log("userId : ", userId)
   const { data: fetchedUser, isLoading } = useUser(userId as string)
-
+  console.log("fetchedUser : ", fetchedUser)
   // if (isLoading || !fetchedUser) {
   //   return (
   //     <div className="flex justify-center items-center h-full">
