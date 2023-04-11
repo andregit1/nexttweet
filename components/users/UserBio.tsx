a5ab431950785dd48fceadf96a704d2e70db0ad6
andregit1 <andreaswidjaya1@gmail.com>
2023-04-11 09:58:21 +0700

    edit modal form

--- components/users/UserBio.tsx
+++ components/users/UserBio.tsx
@@ -6,6 +6,7 @@ import { format } from "date-fns";
 import { useMemo } from "react";
 import Button from "../Button";
 import { BiCalendar } from "react-icons/bi";
+import useEditModal from "@/hooks/useEditModal";
 
 interface UserBioProps {
   userId: string;
@@ -14,6 +15,9 @@ interface UserBioProps {
 function UserBio({ userId }: UserBioProps) {
   const { data: currentUser } = useCurrentUser()
   const { data: fetchedUser } = useUser(userId)
+
+  const editModal = useEditModal()
+
   const createdAt = useMemo(() => {
     if (!fetchedUser?.createdAt) return null
 
@@ -24,7 +28,7 @@ function UserBio({ userId }: UserBioProps) {
     <div className="border-b-[1px] border-neutral-800 pb-4">
       <div className="flex justify-end p-2">
         {currentUser?.id === userId ? 
-          (<Button secondary label="Edit" onClick={() => {}} />) : 
+          (<Button secondary label="Edit" onClick={editModal.onOpen} />) : 
           (<Button secondary label="Follow" onClick={() => {}} />)}
       </div>
 
